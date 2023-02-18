12891cfc212c0e317bbd765df343029b2fb7d127
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-17 18:28:58 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c8-print_base16.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/*
- *prints all the numbers of base 16 in lowercase, followed by a new line.
- */
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int d = 48;
