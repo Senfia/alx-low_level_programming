9c38d210b8741e0c1e8179601d2f7cbb23b94faf
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-17 17:55:08 -0800

    Preprocessor

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ *prints all possible combinations of single-digit numbers
+ */
+
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+		if (d != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
