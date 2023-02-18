9e00ed61f6384ad3f5838bb54f64e9416e78bcf2
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-17 17:06:19 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	char chA;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+	for (chA = 'A'; chA <= 'Z'; chA++)
+{
+        putchar(chA);
+}
+	putchar('\n');
+	return (0);
+}
