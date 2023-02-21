9da940277a35b6043e7eaeb3248c7da94fe3f675
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-21 13:37:38 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - checks lowercase character
+ *
+ * @c : - the lower passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c);
+{
+	char ch;
+
+	if (ch >= 'a' && ch <= 'z')
+
+	return (1);
+}
