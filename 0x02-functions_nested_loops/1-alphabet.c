1acdaf48fd21415fc35dbb1d17965b536f4f976e
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-21 11:12:30 -0800

    prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet(void)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+	_putchar('\n');
+}
