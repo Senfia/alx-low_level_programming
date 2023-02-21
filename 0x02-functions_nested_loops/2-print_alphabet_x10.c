ef54c8643ee59160e882a7fe9b7930f9f6160cdd
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-21 12:55:34 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabets 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (int j = 1; j <= 10; j++)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+	_putchar('\n');
+}
+}
