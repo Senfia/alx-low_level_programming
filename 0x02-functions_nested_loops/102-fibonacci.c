e00a37ac609031a2667cbf4f45fece698e19a816
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-21 16:52:09 -0800

    first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - calculates and prints fibonacci sequence
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	long next, a, b;
+	int counter, start;
+
+	start = 2;
+	counter = 49;
+	a = 1;
+	b = 2;
+	printf("%ld, %ld, ", a, b);
+	while (start < counter)
+	{
+		next = a + b;
+		printf("%ld, ", next);
+		a = b;
+		b = next;
+		start++;
+	}
+	printf("%ld\n", a + b);
+	return (0);
