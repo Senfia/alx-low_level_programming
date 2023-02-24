1d535b0489bcc2de20ac98b1c9eac937d7ecc560
Senyo Fiawornu <contactsenyo@gmail.com>
2023-02-23 16:50:07 -0800

    finds and prints the largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+/**
+ * main - get biggest prime factor of given a number
+ * Return: 0
+ */
+int main(void)
+{
+	long int c, i;
+
+	i = 612852475143;
+	c = 2;
+	while (d < i)
+	{
+		if ((i % c) == 0)
+		{
+			i = i / c;
+			c--;
+		}
+		c++;
+	}
+	printf("%li\i", i);
+
+	return (0);
+}
