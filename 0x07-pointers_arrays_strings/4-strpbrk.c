0143c07d56f6a9cd4286de3f82658fbc7bd5980b
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-06 18:05:49 +0000

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strpbrk - own version of strpbrk()
+ *
+ * @s: string to be analysed
+ * @accept: characters to look for
+ * Return: a pointer to byte in s, NULL if failed
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *temp;
+
+	temp = accept;
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+				return (s);
+			accept++;
+		}
+		accept = temp;
+		s++;
+	}
+	if (*s == '\0')
+		return ('\0');
+	return (s);
