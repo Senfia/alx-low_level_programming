1acdb2728ae82726428a5345c63992322bab18cf
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-14 20:51:20 +0000

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+* simple_print_buffer - prints buffer in hexadecimal
+* @buffer: memory of address to print
+* @size: size of the memory to print
+*
+* Return: Nothing.
+*/
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
