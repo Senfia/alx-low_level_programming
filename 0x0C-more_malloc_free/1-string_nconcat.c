0ddddc8f8f793767903e47590c0539199e89ec5a
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-16 21:14:00 +0000

    string contact

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ *
+ * @str: a given string
+ * Return: the length of the string
+ */
+
+int _strlen(char *str)
+{
+	unsigned int n;
+
+	for (n = 0; *str != '\0'; str++)
+		n++;
+	return (n);
+}
+/**
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: unsigned int representing amount of s2 to copy
+ * Return: pointer to beginning of contact string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len, i, j;
+	char *buff;
+
+	i = 0, j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len = _strlen(s2);
+	if (n > len)
+		n = len;
+	buff = malloc(_strlen(s1) + n + 1);
+	if (buff == NULL)
+		return (NULL);
+	for (i = 0, j = 0; *(s1 + i) != '\0'; i++, j++)
+		*(buff + i) = *(s1 + i);
+	for (i = 0; i < n; i++, j++)
+		*(buff + j) = *(s2 + i);
+	buff[j] = '\0';
+	return (buff);
+}
