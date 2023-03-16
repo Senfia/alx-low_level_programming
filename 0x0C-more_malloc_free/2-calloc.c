b168c52258097f0660bdb86fd10d364a0ca6b054
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-16 21:48:37 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,22 +8,18 @@
  *
  * Return: pointer to memory
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
-	unsigned char *s;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	p = malloc(size * nmemb);
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-
-	s = p;
-	for (i = 0; i < nmemb; i++)
-		p + 1 = 0;
-
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 	return (p);
 }
