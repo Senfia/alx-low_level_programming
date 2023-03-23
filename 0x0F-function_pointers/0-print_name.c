0d8d2a1b6cd3978b89be8ed9ade676b163f76caa
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-23 00:02:12 +0000

    first commit for function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <function_pinters.h>
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to printing fuction
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		exit(98);
+	f(name);
+}
