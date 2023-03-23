21ecd6a70d8fb8d90646fed1583e903046274a85
Senyo Fiawornu <contactsenyo@gmail.com>
2023-03-23 20:47:53 +0000

    updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,20 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+* struct type - for calling printf function for printing
+* @a: format specifier
+* @f: formatter
+* @typ: pointer to function for printf()
+**/
+typedef struct type
+{
+	char a;
+	char *f;
+	void (*typ)(char *, va_list);
+} type;
+
+void print_all(const char * const format, ...);
+
+
 #endif
