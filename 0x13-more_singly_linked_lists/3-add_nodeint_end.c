43dc18b67d281ce1a9b5a1f6aa94a4deadb2be1b
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-03 22:02:37 +0000

    fixed struct declaration

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,35 +3,36 @@
 #include <stdlib.h>
 
 /**
-* *add_nodeint_end - adds a new node at the end of a listint_t list
+* add_nodeint_end - adds a new node at the end of a listint_t list
 * @head: pointer to listint_t list
 * @n: node to copy to end
 * Return: address of the new element, or NULL
 **/
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_nod = malloc(sizeof(listint_t));
+	listint_t *new, *current_node;
 
-	if (new_nod == NULL)
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
 
-	new_nod->n = n;
-	new_nod->next = NULL;
+	new->n = n;
+	new->next = NULL;
+
+	current_node = *head;
 
 	if (*head == NULL)
 	{
-		*head = new_nod;
-		return (new_nod);
+		*head = new;
+		return (*head);
 	}
 
-	listint_t *current_nod = *head;
-
-	while (current_nod->next != NULL)
+	while (current_node->next != NULL)
 	{
-		current_nod = current_nod->next;
+		current_node = current_node->next;
 	}
 
-	current_node->next = new_nod;
-	return (new_nod);
+	current_node->next = new;
+	return (new);
 }
