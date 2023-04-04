8b1f2ec14f0823f01828f9bfa48d4e550a04522d
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-04 16:13:22 +0000

    corrected betty error

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to head node
+ *
+ * Return: pointer to the first node of the reversed list
+ *
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *prev = NULL;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
