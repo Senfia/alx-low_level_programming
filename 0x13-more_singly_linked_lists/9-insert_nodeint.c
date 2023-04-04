7b52318cb287f2b83f649bb8fe8d8197b29a3dca
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-04 04:49:32 +0000

    fixed betty error

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a particular postion
+ * @head: pointer to head node
+ * @idx: where to insert node
+ * @n: data to insert
+ * Return: address of inserted node, NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *current = *head;
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+}
