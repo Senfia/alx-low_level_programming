9367378c047b12b63227269c055f274e161c486a
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-09 19:07:13 +0000

    fixed betty error

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at given index
+ * @n: unsigned int n
+ * @index: starting from 0
+ * Return: index, -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bi = 1ul << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n |= bi;
+
+	return (0);
+}
