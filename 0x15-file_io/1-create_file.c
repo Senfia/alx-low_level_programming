9c8a0fcb55e2445624075c8dfd43f2edec63d2da
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-10 21:33:20 +0000

    fixed add errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	size_t length;
 	ssize_t nwrite;
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	length = strlen(text_content);
 
