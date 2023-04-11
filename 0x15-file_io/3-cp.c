4684716ff1374829d6c78bc75241b1ebac0666ef
Senyo Fiawornu <contactsenyo@gmail.com>
2023-04-11 00:32:47 +0000

    refactor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * error - error * @argc: number of arguments
- * @cd : error code
- * @msg : message
- * Return: 0 on success or error
- */
-
-void error(char *msg, int cd)
-{
-	dprintf(STDERR_FILENO, "%s\n", msg);
-	exit(cd);
-}
-
-/**
- * main - copies the content of a file to another file, custom function
+ * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: file name to pass
  * Return: 0 on success or error
  */
-
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	char buff[BUFF_SIZE];
-	ssize_t nread;
+	int fd_fr, fd_t, nl_read, bwrite;
+	char buf[BUFF_SIZE];
 
 	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd_fr = open(argv[1], O_RDONLY);
+	if (fd_fr < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+	argv[1]), exit(98);
+
+	fd_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_t < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	while ((nl_read = read(fd_fr, buf, BUFF_SIZE)) > 0)
 	{
-		error("Usage: cp file_from file_to", 97);
+		bwrite = write(fd_t, buf, nl_read);
+		if (bwrite != nl_read)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		error("Error: Can't read from file", 98);
-	}
+	if (nl_read < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]), exit(98);
 
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (close(fd_fr) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_fr), exit(100);
 
-	if (file_to == -1)
-		error("Error: Can't write to file", 99);
-
-	while ((nread = read(file_from, buff, BUFF_SIZE)) > 0)
-		if (write(file_to, buff, nread) != nread)
-			error("Error: Can't write to file", 99);
-
-	if (nread == -1)
-		error("Error: Can't read from file", 98);
-
-	if (close(file_from) == -1)
-		error("Error: Can't close fd", 100);
-
-	if (close(file_to) == -1)
-		error("Error: Can't close fd", 100);
+	if (close(fd_t) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_t), exit(100);
 
 	return (0);
 }
