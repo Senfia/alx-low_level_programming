75429d3dd605fe315d2418658620ee36b0b3e56d
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-19 22:52:32 +0000

    working

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,10 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t a;
+	int i;
 
-	for (a = 0; h != NULL; h = h->next, a++)
-	{
-
-	}
+	for (i = 0; h; h = h->next)
+		++i;
 
 	return (i);
 }
