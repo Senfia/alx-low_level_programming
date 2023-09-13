501100ad9bdfc2a84b8032e2e6c5bfadd5db0120
root <root@DESKTOP-JKHB9QO>
2023-09-13 20:54:55 +0000

    fixed typo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,8 +4,8 @@
 /**
  * print_array - print part of an array
  * @array: array
- * @low: min index to print
- * @high: max index to print
+ * @lo: min index to print
+ * @hi: max index to print
  */
 void print_array(int *array, size_t lo, size_t hi)
 {
@@ -21,7 +21,7 @@ void print_array(int *array, size_t lo, size_t hi)
 
 
 /**
- * binary_search - searches for a value in a sorted 
+ * binary_search - searches for a value in a sorted
  * array of integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: is the number of elements in array
